49870d3d35b2956e61ce5f01ff4f31db97e3e1b4
Martijn Dashorst <martijn.dashorst@gmail.com>
2014-06-30 18:45:00 +0200

    Made script more robust

--- release.sh
+++ release.sh
@@ -14,13 +14,13 @@ major_version=$(expr $current_version : '\(.*\)\..*\..*\-SNAPSHOT')
 minor_version=$(expr $current_version : '.*\.\(.*\)\..*\-SNAPSHOT')
 bugfix_version=$(expr $current_version : '.*\..*\.\(.*\)-SNAPSHOT')
 
-CURRENT_VERSION=$major_version.$minor_version.$bugfix_version
+CURRENT_VERSION=$major_version.$minor_version.$bugfix_version.$(date +"%Y%m%d%H%M")
 NEW_VERSION="$major_version.$minor_version.$(expr $bugfix_version + 1)-SNAPSHOT"
 
 echo "Releasing M2E Settings plugin for Eclipse version $CURRENT_VERSION
-------------------------------------------------------------------
-This script assumes you are running on OS X, it hasn't been tested on any other
-operating systems, and you can bet it won't work on Windows...
+------------------------------------------------------------------------
+This script assumes you are running on OS X, it hasn't been tested on
+any other operating systems, and you can bet it won't work on Windows...
 
 REQUIREMENTS:
 
@@ -33,18 +33,213 @@ echo "
 Current Java version is: $(java -version 2>&1 | tail -n 2 | head -n 1)
 "
 
-echo mvn tycho-versions:set-version -DnewVersion=$CURRENT_VERSION
-echo git commit -U -m "Preparing to release $CURRENT_VERSION"
-echo mvn package
-echo rm -rf site
-echo mv nl.topicus.m2e.settings.feature/target/site .
-echo git add site
-echo git commit -m "Created $CURRENT_VERSION"
-echo git tag $CURRENT_VERSION
-echo mvn tycho-versions:set-version -DnewVersion=$NEW_VERSION
-echo git commit -U -m "Preparing for new development"
+mkdir -p target
+
+echo "Releasing $CURRENT_VERSION" > target/release.log
+
+printf "Performing: 'mvn tycho-versions:set-version -DnewVersion=$CURRENT_VERSION' "
+if ! mvn tycho-versions:set-version -DnewVersion=$CURRENT_VERSION >> target/release.log ;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following command to revert your workspace to the original
+state:
+
+    git checkout .
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'git commit -a -m \"Preparing to release $CURRENT_VERSION\"' "
+echo "git commit -a -m \"Preparing to release $CURRENT_VERSION\"" >> target/release.log
+if ! git commit -a -m "Preparing to release $CURRENT_VERSION" >> target/release.log ;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following command to revert your workspace to the original
+state:
+
+    git checkout .
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'mvn clean package' "
+echo "mvn clean package" >> target/release.log
+if ! mvn clean package >> target/release.log ;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following commands to revert your workspace to the original 
+state:
+
+    rm -rf site
+	git reset HEAD^ --hard
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'rm -rf site' "
+echo "rm -rf site" >> target/release.log
+if ! rm -rf site >> target/release.log ;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following commands to revert your workspace to the original 
+state:
+
+    rm -rf site
+	git reset HEAD^ --hard
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'mv nl.topicus.m2e.settings.feature/target/site .' "
+echo "mv nl.topicus.m2e.settings.feature/target/site ." >> target/release.log;
+if ! mv nl.topicus.m2e.settings.feature/target/site . >> target/release.log;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following commands to revert your workspace to the original 
+state:
+
+    rm -rf site
+	git reset HEAD^ --hard
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'git add site' "
+echo "git add site" >> target/release.log;
+if ! git add site >> target/release.log ;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following commands to revert your workspace to the original 
+state:
+
+    rm -rf site
+	git reset HEAD^ --hard
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'git commit -m \"Created $CURRENT_VERSION\" "
+echo "git commit -m \"Created $CURRENT_VERSION\"" >> target/release.log
+if ! git commit -m "Created $CURRENT_VERSION" >> target/release.log ;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following commands to revert your workspace to the original 
+state:
+
+    rm -rf site
+	git reset HEAD^ --hard
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'git tag $CURRENT_VERSION' "	
+echo "git tag $CURRENT_VERSION" >> target/release.log
+if ! git tag $CURRENT_VERSION >> target/release.log;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following commands to revert your workspace to the original 
+state:
+
+    rm -rf site
+	git reset HEAD^ --hard
+
+Check if the tag was created:
+
+    git tag --list
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'mvn tycho-versions:set-version -DnewVersion=$NEW_VERSION' "	
+echo "mvn tycho-versions:set-version -DnewVersion=$NEW_VERSION" >> target/release.log
+if !mvn tycho-versions:set-version -DnewVersion=$NEW_VERSION >> target/release.log ;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following commands to revert your workspace to the original 
+state:
+
+    rm -rf site
+	git reset HEAD^ --hard
+    git tag -d $CURRENT_VERSION
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
+
+printf "Performing: 'git commit -U -m \"Preparing for new development\"' "
+echo "git commit -U -m \"Preparing for new development\"" >> target/release.log
+if ! git commit -U -m "Preparing for new development" >> target/release.log ;
+then
+	echo "FAILED...
+
+See target/release.log for more information.
+
+Use the following commands to revert your workspace to the original 
+state:
+
+    rm -rf site
+	git reset HEAD^ --hard
+    git tag -d $CURRENT_VERSION
+"
+	exit 1
+else
+	echo DONE...
+	echo "" >> target/release.log
+fi
 
 echo "
 Done
 "
-
